e002ae1109be64cc64495f0a578136d7cea9211d
Heber Sandoval <he3ers@gmail.com>
2018-09-03 23:22:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'you are not bob';
+}
+
+const leastFavoriteCustomer = 'you';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'not me';
+}
